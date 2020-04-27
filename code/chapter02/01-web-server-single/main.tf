347b5f366f28a18b330621976b75dc9282ca6950
arellanog <arellanog@cent.gov.ar>
2020-04-27 15:47:46 -0300

    Agrego web server de una sola instancia

---
+++ code/chapter02/01-web-server-single/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
+}
+
+resource "aws_instance" "example" {
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  ami               = "ami-085925f297f89fce1"
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              busybox httpd -f -p 8080 &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
