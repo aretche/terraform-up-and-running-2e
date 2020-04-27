0b622d2fb78d1610be8cef40d9e972718af76639
arellanog <arellanog@cent.gov.ar>
2020-04-27 15:54:47 -0300

    Agrego web server de una sola instancia usando variables y outputs

---
+++ code/chapter02/02-web-server-vars/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">= 0.12, < 0.13"
+}
+
+provider "aws" {
+  region = "us-east-1"
+
+  # Allow any 2.x version of the AWS provider
+  version = "~> 2.0"
+}
+
+resource "aws_instance" "example" {
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  ami               = "ami-085925f297f89fce1"
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
